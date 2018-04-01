0622f6e5b38e75973906c75fd7dd19071c3b1d1d
Howard Huang <howardhuang96@gmail.com>
2018-03-31 23:57:20 -0700

    add trips

--- server/services/models.py
+++ server/services/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 
+class Trip(models.Model):
+    owner = models.ForeignKey('auth.User', related_name='trip', on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+
+
 class TripItem(models.Model):
+    trip = models.ForeignKey(Trip, related_name='trip_items', on_delete=models.CASCADE)
     title = models.CharField(max_length=100, blank=True, default='')
     longitude = models.DecimalField(max_digits=9, decimal_places=6)
     latitude = models.DecimalField(max_digits=9, decimal_places=6)
--- server/services/serializers.py
+++ server/services/serializers.py
@@ -1,7 +1,24 @@
-from services.models import TripItem
+from django.contrib.auth.models import User
+
+from services.models import TripItem, Trip
 from rest_framework import serializers
 
+class TripSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+
+    class Meta:
+        model = Trip
+        fields = ('name', 'owner')
+
 class TripItemSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = TripItem
-        fields = ('title', 'longitude', 'latitude')
+        fields = ('title', 'longitude', 'latitude', 'trip')
+
+class UserSerializer(serializers.ModelSerializer):
+    trip_items = serializers.PrimaryKeyRelatedField(many=True, queryset=TripItem.objects.all())
+
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'trip_items')
