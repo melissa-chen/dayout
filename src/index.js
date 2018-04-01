37d2663efb46e550b421c4f6f0616507c00bdb23
Melissa Chen <melissachen2010@hotmail.com>
2018-03-31 17:16:45 -0700

    boilerplate react code

---
+++ src/index.js
@@ -0,0 +1,6 @@
+// Create a new component. Should produce some HTML
+const App = function() {
+    return <div> Hi! </div>;
+}
+
+// Take this component's generated HTML and put it on the page (in the DOM)
